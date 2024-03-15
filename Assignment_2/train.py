ec1b8a357ad90ba28e4a18454ffdf94f61f0f596
Sushant Gautam <sushantg@ulrik.uio.no>
2024-03-15 15:35:53 +0100

    Update train.py

--- Assignment_2/train.py
+++ Assignment_2/train.py
@@ -10,7 +10,8 @@ def main():
     parser.add_argument('--target_langs', nargs='+', help='Target languages for training and evaluation', required=True)
     parser.add_argument('--model_name', help='Model name or path', required=True)
     parser.add_argument('--batch_size', type=int, help='Batch size for training', default=64)
-    parser.add_argument('--learning_rate', type=float, help='Learning rate for training', default=None)
+    parser.add_argument('--epoch', type=int, help='Number of epochs for training', default=20)
+    parser.add_argument('--learning_rate', type=float, help='Learning rate for training', default="1e-5")
     parser.add_argument('--finetune', action='store_true', help='Flag to finetune the model. If not set, will freeze the transformer layers.')
     parser.add_argument('--separate_val', action='store_true', help='Flag to use separated validation for each language for Task 2. Also saves the plots.')
 
